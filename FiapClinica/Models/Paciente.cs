a4d8e70c907f69cd96b4d84a7ea13ad48b056465
Arthur Thomas <athomasmariano@gmail.com>
2026-03-16 17:33:57 -0300

    feat: cria entidade Paciente com Data Annotations para mapeamento ORM

---
+++ FiapClinica/Models/Paciente.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FiapClinica.Models;
+
+[Table("TB_PACIENTES")]
+public class Paciente
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "O nome é obrigatório.")]
+    [StringLength(100)]
+    public string Nome { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "O CPF é obrigatório.")]
+    [StringLength(11)]
+    public string Cpf { get; set; } = string.Empty;
+
+    [Required]
+    public DateTime DataNascimento { get; set; }
+
+    [Required]
+    [EmailAddress]
+    [StringLength(150)]
+    public string Email { get; set; } = string.Empty;
+}
