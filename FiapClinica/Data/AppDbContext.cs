c3d201a4247fd386681831a36f10780a938ff7d7
Arthur Thomas <athomasmariano@gmail.com>
2026-03-16 17:35:15 -0300

    feat: adiciona AppDbContext e registra DbSet de Pacientes

---
+++ FiapClinica/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FiapClinica.Models;
+
+namespace FiapClinica.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Paciente> Pacientes { get; set; }
+}
