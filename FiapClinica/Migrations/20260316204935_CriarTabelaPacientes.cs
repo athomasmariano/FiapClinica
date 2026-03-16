5b2bb06b8f56749513c0b46f37d3cce14552bb4e
Arthur Thomas <athomasmariano@gmail.com>
2026-03-16 17:51:05 -0300

    feat: gera migration inicial e cria tabela TB_PACIENTES no banco Oracle

---
+++ FiapClinica/Migrations/20260316204935_CriarTabelaPacientes.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FiapClinica.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriarTabelaPacientes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TB_PACIENTES",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                        .Annotation("Oracle:Identity", "START WITH 1 INCREMENT BY 1"),
+                    Nome = table.Column<string>(type: "NVARCHAR2(100)", maxLength: 100, nullable: false),
+                    Cpf = table.Column<string>(type: "NVARCHAR2(11)", maxLength: 11, nullable: false),
+                    DataNascimento = table.Column<DateTime>(type: "TIMESTAMP(7)", nullable: false),
+                    Email = table.Column<string>(type: "NVARCHAR2(150)", maxLength: 150, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TB_PACIENTES", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TB_PACIENTES");
+        }
+    }
+}
